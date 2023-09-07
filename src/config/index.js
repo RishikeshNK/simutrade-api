84f8e92853e96f11ffb690689f0b3d665631a039
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-09-07 16:00:19 -0400

    feat: start over

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-const config = {
-  PORT: process.env.PORT || 3000,
-  MONGO_URI: process.env.MONGO_URI
-}
-
-module.exports = config
--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const logger = require('./utils/logger')
-const connect = require('./utils/connect')
-const config = require('./config')
-
-app.listen(config.PORT, () => {
-  logger.info(`Simutrade API is listening on port ${config.PORT}`)
-  connect()
-})
