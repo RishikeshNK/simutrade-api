0e29f2dda46c33cee4585fd33d50424634b541d6
Rishikesh <rishikesh.kanabar@gmail.com>
2023-09-29 12:46:02 -0400

    feat: add passport auth

---
+++ src/config/app.ts
@@ -0,0 +1,5 @@
+import "dotenv/config"
+
+export default {
+    JWT_SECRET: process.env.JWT_SECRET
+}
---
+++ src/config/passport.ts
@@ -0,0 +1,60 @@
+import type { Express } from "express";
+import passport from "passport";
+import passportLocal from "passport-local";
+import passportJWT from "passport-jwt";
+import prisma from "./prisma";
+import appConfig from "./app";
+import { comparePassword } from "../utils/password";
+
+const LocalStrategy = passportLocal.Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+export const configure = (app: Express) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(
+    "login",
+    new LocalStrategy(
+      { usernameField: "email", passwordField: "password" },
+      (email, password, done) => {
+        return prisma.user
+          .findUnique({ where: { email } })
+          .then(async (user) => {
+            if (!user)
+              return done(undefined, false, {
+                message: `Email ${email} not found.`,
+              });
+
+            const isMatch = await comparePassword(password, user.password);
+            if (isMatch) return done(undefined, user);
+            return done(undefined, false, {
+              message: "Invalid email or password",
+            });
+          })
+          .catch((err) => done(err));
+      }
+    )
+  );
+
+  passport.use(
+    new JWTStrategy(
+      {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: appConfig.JWT_SECRET,
+      },
+      (jwtPayload, cb) => {
+        return prisma.user
+          .findUnique({ where: { id: jwtPayload.id } })
+          .then((user) => {
+            if (!user) throw new Error("User not found");
+            return cb(null, user);
+          })
+          .catch((err) => {
+            return cb(err);
+          });
+      }
+    )
+  );
+};
